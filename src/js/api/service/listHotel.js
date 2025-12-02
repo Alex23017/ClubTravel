eec034e74b4643804af3b703ec9cb526c1cc9c4a
Alex Yavorskyi <entertains1997@gmail.com>
2025-12-02 15:28:39 +0200

    Сохраняю текущие изменения перед pull

--- src/js/api/service/listHotel.js
+++ src/js/api/service/listHotel.js
@@ -3,8 +3,16 @@ import { getResource } from '../api'
 
 export async function getListHotel() {
   try {
-    const res = await getResource(
-      `${API_VARIABLES.BASE_URL}/api/list-hotels?populate=openList.openListSelect`)
+    const res = await getResource(`${API_VARIABLES.BASE_URL}/api/list-hotels?populate=openList.openListSelect`)
+
+    return res.data
+  } catch (error) {
+    console.error('Error fetching list hotels:', error)
+  }
+}
+export async function getListHotelMob() {
+  try {
+    const res = await getResource(`${API_VARIABLES.BASE_URL}/api/list-hotels?populate=*`)
 
     return res.data
   } catch (error) {
