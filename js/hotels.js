484b395d1b1e7794ee6b457b0176b4405eabcbcd
Alex Yavorskyi <entertains1997@gmail.com>
2025-12-14 14:00:16 +0200

    Updates

--- js/hotels.js
+++ js/hotels.js
@@ -2,7 +2,9 @@ import { A as API_VARIABLES } from "./variables.js";
 import { g as getPublicResource } from "./api.js";
 async function getHotelById(id) {
   try {
-    const res = await getPublicResource(`${API_VARIABLES.BASE_URL}/api/list-hotels/${id}?populate[openList][populate][openListSelect]=true&populate[img]=true`);
+    const res = await getPublicResource(
+      `${API_VARIABLES.BASE_URL}/api/list-hotels/${id}?populate[serviceLists][populate][list][populate][subServices]=*&populate[serviceLists][populate][position]=*&populate[serviceLists][populate][listPosition]=*`
+    );
     return res.data;
   } catch (error) {
     console.error(`Error fetching hotel with id ${id}:`, error);
