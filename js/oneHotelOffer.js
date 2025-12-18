43dc1a3a04d24f03272d4105bfdfb2823be6df18
Alex Yavorskyi <entertains1997@gmail.com>
2025-12-18 20:20:41 +0200

    Updates

--- js/oneHotelOffer.js
+++ js/oneHotelOffer.js
@@ -5156,26 +5156,6 @@ function skeleton(containerSkeleton, selector) {
     }, 1e3);
   });
 }
-function renderSkeleton(block, count) {
-  const container = document.querySelector(block);
-  if (!container) return;
-  container.innerHTML = "";
-  for (let i = 0; i < count; i++) {
-    const slide = document.createElement("div");
-    slide.classList.add("skeleton__card", "swiper-slide");
-    slide.innerHTML = `
-      <div class="skeleton__img"></div>
-      <div class="skeleton__data"></div>
-      <div class="skeleton__count"></div>
-      <div class="skeleton__line"></div>
-    `;
-    container.appendChild(slide);
-  }
-}
-renderSkeleton(".news__container", 4);
-renderSkeleton(".hotdeals__container", 4);
-renderSkeleton(".wintertours__container", 5);
-renderSkeleton(".summertours__container", 5);
 document.addEventListener("DOMContentLoaded", () => {
   function isLoggedIn() {
     return localStorage.getItem("Logged") === "true";
